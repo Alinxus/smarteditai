2ff1870214169c7602aa51174d2ca0c335e30291
Al-ameen Olajide <81990934+Alinxus@users.noreply.github.com>
2024-01-26 19:36:35 +0000

    new

--- smarteditai/auth.js
+++
@@ -1,4 +0,0 @@
-const NextAuth = require('next-auth');
-const NextAuthConfig = require('./next-auth.config.js');
- 
-module.exports = (req, res) => NextAuth(req, res, NextAuthConfig);
--- smarteditai/components/elements/customLink.js
+++
@@ -1,7 +0,0 @@
-export default function CustomLink({ link, text }) {
-  return (
-    <a href={link} className="block md:px-4 transition hover:text-primary">
-      <span>{text}</span>
-    </a>
-  );
-}
